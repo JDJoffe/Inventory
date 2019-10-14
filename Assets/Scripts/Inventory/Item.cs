b04a7569dd2a354bfac31e7c5214a36d2f0223d4
josh <48034855+JDJoffe@users.noreply.github.com>
2019-10-14 16:02:05 +1100

    initial

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Item 
+{
+    #region Private Var
+    string _name;
+    string _desctiption;
+    int _value;
+    int _id;
+    int _amount;
+    int _damage;
+    int _durability;
+    int _armour;
+    int _heal;
+    Texture2D _icon;
+    GameObject _mesh;
+    ItemType _type;
+    #endregion
+    #region Public Prop
+    public string Name { get { return _name; } set { _name = value; } }
+    public string Desctiption { get { return _desctiption; } set { _desctiption = value; } }
+    public int ID { get { return _id; } set { _id = value; } }
+    public int Value { get { return _value; } set { _value = value; } }
+    public int Amount { get { return _amount; } set { _amount = value; } }
+    public int Damage { get { return _damage; } set { _damage = value; } }
+    public int Durability { get { return _durability; } set { _durability = value; } }
+    public int Armour { get { return _armour; } set { _armour = value; } }
+    public int Heal { get { return _heal; } set { _heal = value; } }
+    public Texture2D Icon { get { return _icon; } set { _icon = value; } }
+    public GameObject ItemMesh { get { return _mesh; } set { _mesh = value; } }
+    public ItemType Type { get { return _type; } set { _type = value; } }
+    #endregion
+}
+#region Enum
+public enum ItemType
+{
+    Apparrel,
+    Consumable,
+    Weapon,
+    Potion,
+    Food,
+    Material,
+    Scroll,
+    Quest,
+    Money,
+    Misc
+}
+#endregion
---
+++ Assets/Scripts/linear/Inventory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Linear
+{
+    public class Inventory : MonoBehaviour
+    {
+        #region var
+        public List<Item> inv = new List<Item>();
+        public Item selectedItem;
+        public bool showInv;
+        public Vector2 scr;
+        public Vector2 scrollpos;
+        public int money;
+        public string sortType = "";
+        public Transform dropLocation;
+        [System.Serializable]
+        public struct equipment
+        {
+            public string name;
+            public Transform location;
+            public GameObject curItem;
+        };
+        public equipment[] equipmentSlots;
+        #endregion
+        // Start is called before the first frame update
+        void Start()
+        {
+            inv.Add(ItemData.CreateItem(0));
+            inv.Add(ItemData.CreateItem(2));
+            inv.Add(ItemData.CreateItem(27));
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                showInv = !showInv;
+                if (showInv)
+                {
+                    Time.timeScale = 0;
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    return;
+                }
+                else
+                {
+                    Time.timeScale = 1;
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    return;
+                }
+            }
+        }
+        private void OnGUI()
+        {
+            if (showInv)
+            {
+                // you would normally put this in an options menu
+                scr.x = Screen.width / 16;
+                scr.y = Screen.height / 9;
+
+                GUI.Box(new Rect(0, 0, scr.x * 8, Screen.height), "");
+            }
+        }
+    }
+}
